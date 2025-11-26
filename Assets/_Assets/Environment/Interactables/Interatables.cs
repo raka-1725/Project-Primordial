8e56b202f8acb2542540155c42a1ef4e16f4895c
Aiden Sosa <aidensosa@Aidens-MBP.lan>
2025-11-25 18:46:40 -0600

    Door Walls and Pressure Plates

--- Assets/_Assets/Environment/Interactables/Interatables.cs
+++ Assets/_Assets/Environment/Interactables/Interatables.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public interface IInteractable
 {
     void Activate(Interactions player);
-    //Key pick up
+    
+}
+
+//Key pick up
     //Chest or lootables
     //Medkits
     //Levers activate arrays
     //
-
-}
