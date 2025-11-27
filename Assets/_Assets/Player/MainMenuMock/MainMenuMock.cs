f824aa0f5df902b7879292dfa7fec616bd02942e
Akaike <akaike@student.uiwtx.edu>
2025-11-26 19:07:30 -0600

    MainMenu interaction and UI inprogress

---
+++ Assets/_Assets/Player/MainMenuMock/MainMenuMock.cs
@@ -0,0 +1,36 @@
+using Unity.AppUI.UI;
+using UnityEngine;
+
+public class MainMenuMock : MonoBehaviour
+{
+    Animator mAnimator;
+    ButtonHover[] mButtons;
+
+    private void Awake()
+    {
+        mAnimator = GetComponent<Animator>();
+
+        mButtons = FindObjectsByType<ButtonHover>(FindObjectsSortMode.None);
+        foreach (ButtonHover BHover in mButtons)
+        {
+            BHover.onHoverChanged += HoverOverButtion;
+        }
+
+    }
+    public void HoverOverButtion(bool hover) 
+    {
+        mAnimator.SetBool("ButtonHover", hover);
+    }
+
+    public void StartScene() 
+    {
+        mAnimator.SetTrigger("StartGame");
+    }
+
+    public void FinishAnim() 
+    {
+
+        AsyncLoader asyncLoader = FindAnyObjectByType<AsyncLoader>();
+        asyncLoader.LoadLevel("Maze");
+    }
+}
